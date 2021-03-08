c6eebe93c51c6773d283bc1fbf974c860d5b553f
Orkhan Mustafayev <orkhanmustafayev44@gmail.com>
2021-03-08 19:50:39 +0400

    Meqalenin Linki qoyuldu

---
+++ Lahiye/BackEnd/run.py
@@ -0,0 +1,8 @@
+from flask import Template
+t = Template("Hello {{ something }}!")
+t.render(something="World")
+u'Hello World!'
+
+t = Template("My favorite numbers: {% for n in range(1,10) %}{{n}} " "{% endfor %}")
+t.render()
+u'My favorite numbers: 1 2 3 4 5 6 7 8 9 '
